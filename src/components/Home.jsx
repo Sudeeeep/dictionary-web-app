c0aaf31e761add3c0846bf3bd7efe0c6b951615f
Sudeep <sudeepn15@gmail.com>
2023-06-26 21:06:14 +0530

    feat: add home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import bookLogo from "../assets/book.png";
+
+export const Home = () => {
+  return (
+    <div className="text-center mt-28">
+      <img src={bookLogo} alt="" className="w-20 m-auto" />
+      <p>Enter the word you want to search in the search bar</p>
+    </div>
+  );
+};
